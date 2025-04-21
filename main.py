b688bc0876e2fc72625ec494d45fb5828c1a4e4b
Isaac Awotwe <isaacawotwe@gmail.com>
2025-04-21 00:10:33 -0600

    Update script

--- main.py
+++ main.py
@@ -1,3 +1,24 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+menu_obj = Menu()
+coffee = CoffeeMaker()
+money = MoneyMachine()
+
+machine_on = True
+
+while machine_on:
+    choice = input(f"What would you like? ({menu_obj.get_items()}): ")
+    if choice == "off":
+        machine_on = False
+    elif choice == "report":
+        coffee.report()
+        money.report()
+    else:
+        drink = menu_obj.find_drink(choice)
+        enough_resources = coffee.is_resource_sufficient(drink)
+        if enough_resources:
+            enough_money = money.make_payment(drink.cost)
+            if enough_money:
+                coffee.make_coffee(drink)
